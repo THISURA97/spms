a30e9cdbbbff31ab0c11024797c0e53e3303b378
thisura97 <tdinith481@gmail.com>
2020-12-07 22:30:27 +0530

    redux config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Root from "./Root"; // <------------- new import
+import { Route, Switch } from "react-router-dom"; // <--- remove BrowserRouter
 import Home from "./components/Home";
 import Signup from "./components/signup/Signup";
 import Login from "./components/login/Login";
@@ -9,14 +10,15 @@ class App extends Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
+        <Root> {/* replace BrowserRouter with Root */}
           <Switch>
             <Route path="/signup" component={Signup} />
             <Route path="/login" component={Login} />
             <Route path="/dashboard" component={Dashboard} />
             <Route exact path="/" component={Home} />
+            <Route path="*">Ups</Route>
           </Switch>
-        </BrowserRouter>
+        </Root> {/* replace BrowserRouter with Root */}
       </div>
     );
   }
