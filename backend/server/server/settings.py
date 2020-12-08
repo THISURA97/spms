ca3df9fb57306dc03ba226c6ec3cdd13d73da656
thisura97 <tdinith481@gmail.com>
2020-12-08 13:29:16 +0530

    Complete Login and Signup func.

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
-    'apps.accounts'
+    'apps.accounts',
+    'corsheaders'
 ]
 #configure DRF
 REST_FRAMEWORK = {
@@ -65,6 +66,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'server.urls'
@@ -136,3 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# define which origins are allowed
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 import Root from "./Root"; // <------------- new import
 import { Route, Switch } from "react-router-dom"; // <--- remove BrowserRouter
+import { ToastContainer } from "react-toastify";
 import Home from "./components/Home";
 import Signup from "./components/signup/Signup";
 import Login from "./components/login/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 
+
+import axios from "axios";
+axios.defaults.baseURL = "http://127.0.0.1:8000";
 class App extends Component {
   render() {
     return (
       <div>
         <Root> {/* replace BrowserRouter with Root */}
+          <ToastContainer hideProgressBar={true} newestOnTop={true} />
           <Switch>
             <Route path="/signup" component={Signup} />
             <Route path="/login" component={Login} />
@@ -19,6 +24,7 @@ class App extends Component {
             <Route path="*">Ups</Route>
           </Switch>
         </Root> {/* replace BrowserRouter with Root */}
+
       </div>
     );
   }
